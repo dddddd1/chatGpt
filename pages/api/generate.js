183cfc64a734ba8befd8add37ffce4d3580a39e4
bdeng <bdeng@bdengdeMBP.lan>
2023-02-14 10:50:40 +0800

    n

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,22 +15,24 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  // const animal = req.body.animal || '';
+  // if (animal.trim().length === 0) {
+  //   res.status(400).json({
+  //     error: {
+  //       message: "Please enter a valid animal",
+  //     }
+  //   });
+  //   return;
+  // }
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      prompt: req.body.data,
+      max_tokens: 700,
+      temperature: 0,
     });
+    console.log(completion.data.choices, "completion ", req.body.data)
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+// import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ data: animalInput }),
       });
 
       const data = await response.json();
@@ -23,7 +23,7 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      // setAnimalInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -33,25 +33,26 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
+      {/* <Head>
         <title>OpenAI Quickstart</title>
         <link rel="icon" href="/dog.png" />
-      </Head>
+      </Head> */}
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>提问</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="请输入你的问题"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="提交" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result} dangerouslySetInnerHTML={{ __html: result }} />
+        {/* <div className={styles.result} dangerouslySetInnerHTMl = {{__html:result}}></div> */}
       </main>
     </div>
   );
